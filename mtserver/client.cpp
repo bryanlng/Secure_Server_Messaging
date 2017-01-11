36747f93cb8dfe5a430a70df329390cad425269d
Bryan Leung <bleungtx2@gmail.com>
2017-01-11 11:33:12 -0700

    Tried to do something with the client, where it would generate a formatted version of the message, given the original message that the user sends

--- mtserver/client.cpp
+++ mtserver/client.cpp
@@ -18,12 +18,45 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+   http://en.cppreference.com/w/cpp/chrono/c/time
 */
 
+#include <iostream>
+#include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <ctime>
 #include "tcpconnector.h"
+
+/*
+	Generates the required format
+	timestamp <delimiter> date_formatted <delimiter> message <delimiter>
+*/
+string formatMessage(string message, string delimiter) {
+	string result;
+
+	//1. Generate timestamp, then concatenate onto our string
+	std::time_t timer = std::time(NULL);
+	long millis = static_cast<long>(timer);
+	std::stringstream sstm;
+	sstm << millis;
+	result = sstm.str();
+	result += delimiter;
+
+	//2. Generate current date, but formatted, then concatenate
+	char* formatted = std::asctime(std::localtime(&timer));
+	string temp(formatted);
+	result += temp;
+	result += delimiter;
+
+	//3. Add on message
+	result += message;
+	result += delimiter;
+
+	return result;
+}
 
 int main(int argc, char** argv)
 {
@@ -36,11 +69,17 @@ int main(int argc, char** argv)
 
     int len;
     string message;
+	string delimiter = ":";
+	string temp(argv[3]);
+	string test = (temp, delimiter);
+	std::cout << "Test: " << test << std::endl;
+	string formatted;
     char line[256];
     TCPConnector* connector = new TCPConnector();
     TCPStream* stream = connector->connect(argv[2], atoi(argv[1]));
     if (stream) {
 		message = std::string(argv[3]);
+		//message = (message, delimiter);
 		stream->send(message.c_str(), message.size());
 		printf("sent - %s\n", message.c_str());
 		len = stream->receive(line, sizeof(line));
