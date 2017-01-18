201fb91e98c767cbfb0210bff17faf33cedcabcc
Bryan Leung <bleungtx2@gmail.com>
2017-01-17 18:31:21 -0700

    Added Readers-Writers solution for ThreadSafeFile's read() and write(). Not sure if it works, but it's there

--- mtserver/ThreadSafeFile.cpp
+++ mtserver/ThreadSafeFile.cpp
@@ -1,8 +1,16 @@
 #include "ThreadSafeFile.h"
 
 ThreadSafeFile::ThreadSafeFile(std::ofstream& ofs) : file(ofs) {
+	//Initialize locks and condition variables
 	pthread_mutex_init(&lock, NULL);
-	pthread_cond_init(&cond_var, NULL);
+	pthread_cond_init(&read_cond_var, NULL);
+	pthread_cond_init(&write_cond_var, NULL);
+
+	//Intialize fields
+	active_readers = 0;
+	active_writers = 0;
+	waiting_readers = 0;
+	waiting_writers = 0;
 }
 
 std::ofstream& ThreadSafeFile::getFileStream() {
@@ -16,15 +24,52 @@ void ThreadSafeFile::open(const char* filename) {
 }
 
 void ThreadSafeFile::read() {
-	pthread_mutex_lock(&lock);
+	pthread_mutex_lock(&lock);		//always lock before doing anything
 
-	pthread_mutex_unlock(&lock);
+	//Part 1: Begin read
+	while (active_writers == 1 || waiting_writers > 0) {
+		++waiting_readers;
+		pthread_cond_wait(&read_cond_var, &lock);
+		--waiting_readers;
+	}
+
+	++active_readers;
+	pthread_cond_signal(&read_cond_var);
+
+	//Part 2: The actual read operation
+
+	//Part 3: End read
+	if (--active_readers == 0) {
+		pthread_cond_signal(&write_cond_var);
+	}
+
+	pthread_mutex_unlock(&lock);	//always unlock after doing operations
 }
 
 void ThreadSafeFile::write() {
-	pthread_mutex_lock(&lock);
+	pthread_mutex_lock(&lock);		//always lock before doing anything
 
-	pthread_mutex_unlock(&lock);
+	//Part 1: Begin write
+	while (active_writers == 1 || waiting_readers > 0) {
+		++waiting_writers;
+		pthread_cond_wait(&write_cond_var, &lock);
+		--waiting_writers;
+	}
+
+	++active_writers;
+	
+	//Part 2: The actual write operation
+
+	//Part 3: End write
+	--active_writers;
+	if (waiting_readers) {
+		pthread_cond_signal(&read_cond_var);
+	}
+	else {
+		pthread_cond_signal(&write_cond_var);
+	}
+
+	pthread_mutex_unlock(&lock);	//always unlock after doing operations;
 }
 
 void ThreadSafeFile::close() {
@@ -36,5 +81,6 @@ void ThreadSafeFile::close() {
 
 ThreadSafeFile::~ThreadSafeFile() {
 	pthread_mutex_destroy(&lock);
-	pthread_cond_destroy(&cond_var);
+	pthread_cond_destroy(&read_cond_var);
+	pthread_cond_destroy(&write_cond_var);
 }
--- mtserver/ThreadSafeFile.h
+++ mtserver/ThreadSafeFile.h
@@ -1,16 +1,57 @@
 /*
-	
+	Abstraction over C++'s file operations that allows multiple threads to 
+	open, read, write, and close the file
+
+	Basically, this is the readers writers problem.
+
+	Partial solution provided at:
+	http://tinyurl.com/rwsolution
+	However, I modified it so that we WAIT IN WHILE LOOPS, which you MUST do
+	in order to ensure that we get the correct results, as we could have been
+	robbed along the way.
+
+	The Readers-Writers problem:
+		-Needs to provide a balance between readers and writers
+		-Multiple readers can read at a time, but only 1 writer can write 
+		 at a time
+		-Rules:
+			1. Readers can only read when there's no writers
+			2. Writers can only write when there's no active readers 
+			3. Writer can't enter if 
+
+	Example case scenarios:
+	1. 10 readers come in, then a writer:
+		-10 readers get in
+		-writer waits. And waits. And waits. Until the last reader calls
+		 signal(canWrite)
+	2. 2 readers come in, then 2 writers:
+		
+		
+
+
+	http://www.cplusplus.com/doc/tutorial/files/
 	http://stackoverflow.com/questions/9658720/ofstream-as-function-argument
+	http://stackoverflow.com/questions/7868936/read-file-line-by-line
+
 */
 
 
 #include <fstream>
 class ThreadSafeFile {
 	private:
-		std::ofstream& file;		//the ofstream that represents the file
-		pthread_mutex_t  lock;		//Lock, so that only 1 Thread can edit a File at a time
-		pthread_cond_t   cond_var;	//Condition variable, to signal a waiting Thread that they can
-									//now use the file
+		std::ofstream&	 file;			//the ofstream that represents the file
+		pthread_mutex_t  lock;			//Lock, so that only 1 Thread can edit a File at a time
+		pthread_cond_t   read_cond_var;	//Condition variable, to signal a waiting Thread that they can
+										//now use the file
+		pthread_cond_t   write_cond_var;	//Condition variable, to signal a waiting Thread that they can
+										//now use the file
+
+		int active_readers;				//used so that we can have a condition to wait on
+		int active_writers;				//used so that we can have a condition to wait on
+		int waiting_readers;
+		int waiting_writers;
+
+
 	public:
 		ThreadSafeFile(std::ofstream& ofs);
 		~ThreadSafeFile();
