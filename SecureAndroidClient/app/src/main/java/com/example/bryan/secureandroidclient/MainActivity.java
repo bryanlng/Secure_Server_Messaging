dd1435b0f3a8efe5e256c1968e6ce6949edc3d09
Bryan Leung <bleungtx2@gmail.com>
2017-01-30 01:38:22 -0700

    Added super basic rough design of the android app. No security, no anything. Just trying to get the UI to look eh. Still need to work on the ListView

---
+++ SecureAndroidClient/app/src/main/java/com/example/bryan/secureandroidclient/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.bryan.secureandroidclient;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+    private MessagesFragment messagesFragment;
+    private EditText chatbox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize MessagesFragment
+        FragmentManager fragmentManager = getFragmentManager();
+        messagesFragment = new MessagesFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_container, messagesFragment);
+        ft.addToBackStack(null);
+        ft.commit();
+        fragmentManager.executePendingTransactions();
+
+        //Initialize chatbox
+        chatbox = (EditText)findViewById(R.id.chatbox);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ SecureAndroidClient/app/src/main/java/com/example/bryan/secureandroidclient/MessagesFragment.java
@@ -0,0 +1,76 @@
+package com.example.bryan.secureandroidclient;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MessagesFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MessagesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MessagesFragment extends Fragment {
+    private ListView messagesListView;
+
+    public MessagesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
+        setRetainInstance(true);
+
+        messagesListView = (ListView)rootView.findViewById(R.id.list);
+
+        ArrayList<String> values = new ArrayList<>();
+        values.add("a");
+        values.add("b");
+        values.add("ccccccccccccc");
+        values.add("ddddddddddddddd");
+        values.add("eeeeeeeeeeeeeeeeeeeeeee");
+        values.add("fffff");
+        values.add("ggggggggg");
+        values.add("hhhhhhhhh");
+        values.add("iiiiiiiiiii");
+        values.add("jjjjjjjjjjjjjjj");
+        values.add("kkkkkkkkkk");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, values);
+
+        messagesListView.setAdapter(adapter);
+
+        messagesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(view.getContext(), "Item selected: " + item, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+}
